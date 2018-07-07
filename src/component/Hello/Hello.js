94f784922e228af34a8bbdc88f04c31b0dc00ba0
hm.guo <1982862270@qq.com>
2018-07-07 21:03:58 +0800

    第一次提交，写到redux那里

---
+++ src/component/Hello/Hello.js
@@ -0,0 +1,9 @@
+import React,{Component} from 'react';
+
+export default class Hello extends React.Component{
+    render(){
+        return(
+            <div>Hello React!</div>
+        )
+    }
+}
---
+++ webpack.dev.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+module.exports = {
+
+    // 入口
+    entry: [
+        // 用来保持react中的state
+        'react-hot-loader/patch',
+
+        path.join(__dirname, 'src/index.js')
+    ],
+    // 输出到dist文件夹，输出文件名字为bundle.js
+    output: {
+        path: path.join(__dirname, './dist'),
+        filename: 'bundle.js'
+    },
+    mode: 'development',
+    module: {
+        /*src文件夹下面的以.js结尾的文件，要使用babel解析*/
+        /*cacheDirectory是用来缓存编译结果，下次编译加速*/
+        rules: [{
+            test: /\.js$/,
+            use: ['babel-loader?cacheDirectory=true'],
+            include: path.join(__dirname, 'src')
+        }]
+    },
+    // webpack-dev-server就是一个小型的静态文件服务器。使用它，可以为webpack打包生成的资源文件提供Web服务
+    devServer: {
+        contentBase: path.join(__dirname, './dist'),
+        port: 3001,
+        historyApiFallback: true,
+        hot: true
+    },
+    // 别名
+    resolve:{
+        alias:{
+            pages:path.join(__dirname,'src/pages'),
+            component:path.join(__dirname,'src/component'),
+            router:path.join(__dirname,'src/router')
+        }
+    }
+}
