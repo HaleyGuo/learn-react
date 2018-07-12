853bb294b8329d5b5d55968cdb9afd128fe48364
hm.guo <1982862270@qq.com>
2018-07-12 19:49:51 +0800

    使用redux

---
+++ src/redux/actions/counter.js
@@ -0,0 +1,16 @@
+// actions/counter.js  先来写action创建函数。通过action创建函数，可以创建action~
+export const INCREMENT = "counter/INCREMENT";
+export const DECREMENT = "counter/DECREMENT";
+export const RESET = "counter/RESET";
+
+export function increment(){
+    return {type:INCREMENT};
+}
+
+export function decrement() {
+    return {type: DECREMENT}
+}
+
+export function reset() {
+    return {type: RESET}
+}
