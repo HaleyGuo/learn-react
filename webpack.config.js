f8c205e1192db39f40d9567864959f17a48b47f9
hm.guo <1982862270@qq.com>
2018-07-13 16:47:59 +0800

    抽取css

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
-const UglifyJSPlugin=require('uglifyjs-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
     devtool: 'cheap-module-source-map',
@@ -19,22 +20,26 @@ module.exports = {
         publicPath: "/"//让静态文件的链接定位到静态服务器
     },
     module: {
-        rules: [{
-            test: /\.js$/,
-            use: ['babel-loader'],
-            include: path.join(__dirname, 'src')
-        }, {
-            test: /\.css$/,
-            use: ['style-loader', 'css-loader']
-        }, {
-            test: /\.(png|jpg|gif)$/,
-            use: [{
-                loader: 'url-loader',
-                options: {
-                    limit: 8192
-                }
+        rules: [
+            {
+                test: /\.js$/,
+                use: ['babel-loader'],
+                include: path.join(__dirname, 'src')
+            }, {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback:"style-loader",
+                    use: "css-loader"
+                })
+            }, {
+                test: /\.(png|jpg|gif)$/,
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        limit: 8192
+                    }
+                }]
             }]
-        }]
     },
     plugins: [
         new HtmlWebpackPlugin({
@@ -44,20 +49,24 @@ module.exports = {
         new UglifyJSPlugin(),
         // library 将通过与 process.env.NODE_ENV 环境变量关联，以决定 library 中应该引用哪些内容
         new webpack.DefinePlugin({
-            'process.env':{
-                'NODE_ENV':JSON.stringify('production')
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production')
             }
         }),
         new webpack.HashedModuleIdsPlugin(),
-        new CleanWebpackPlugin(['dist'])
+        new CleanWebpackPlugin(['dist']),
+        new ExtractTextPlugin({
+            filename:'[name].[hash:5].css',
+            allChunks:true
+        })
 
     ],
     optimization: {
-        splitChunks:{
+        splitChunks: {
             name: 'vendor'
         },
         runtimeChunk: {
-            name:'runtime'
+            name: 'runtime'
         }
     },
 
