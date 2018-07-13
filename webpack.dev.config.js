65ba02e1ae4c09104d60fa775f6061eecae6c110
hm.guo <1982862270@qq.com>
2018-07-13 10:38:50 +0800

    给Page1.js增加样式，并在webpack.dev.config里配置rules编译css

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -17,11 +17,18 @@ module.exports = {
     module: {
         /*src文件夹下面的以.js结尾的文件，要使用babel解析*/
         /*cacheDirectory是用来缓存编译结果，下次编译加速*/
-        rules: [{
-            test: /\.js$/,
-            use: ['babel-loader?cacheDirectory=true'],
-            include: path.join(__dirname, 'src')
-        }]
+        rules: [
+            {
+                test: /\.js$/,
+                use: ['babel-loader?cacheDirectory=true'],
+                include: path.join(__dirname, 'src'),
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+                //include: path.join(__dirname, 'src'),
+            }
+        ]
     },
     // webpack-dev-server就是一个小型的静态文件服务器。使用它，可以为webpack打包生成的资源文件提供Web服务
     devServer: {
@@ -31,16 +38,16 @@ module.exports = {
         hot: true
     },
     // 别名
-    resolve:{
-        alias:{
-            pages:path.join(__dirname,'src/pages'),
-            component:path.join(__dirname,'src/component'),
-            router:path.join(__dirname,'src/router'),
+    resolve: {
+        alias: {
+            pages: path.join(__dirname, 'src/pages'),
+            component: path.join(__dirname, 'src/component'),
+            router: path.join(__dirname, 'src/router'),
 
-            actions:path.join(__dirname,'src/redux/actions'),
-            reducers:path.join(__dirname,'src/redux/reducers'),
+            actions: path.join(__dirname, 'src/redux/actions'),
+            reducers: path.join(__dirname, 'src/redux/reducers'),
         }
     },
 
-    devtool:'inline-source-map',//对控制台报错信息的优化
+    devtool: 'inline-source-map',//对控制台报错信息的优化
 }
